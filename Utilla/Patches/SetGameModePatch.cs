f85be20f052a4483b7c27ec175f1c76661260ade
Cha <113856355+Cha554@users.noreply.github.com>
2026-03-14 20:11:48 -0400

    Add files via upload

---
+++ Utilla/Patches/SetGameModePatch.cs
@@ -0,0 +1,17 @@
+using GorillaNetworking;
+using HarmonyLib;
+
+namespace Utilla.Patches
+{
+    [HarmonyPatch(typeof(GorillaComputer), nameof(GorillaComputer.SetGameModeWithoutButton))]
+    internal static class SetGameModePatch
+    {
+        public static bool PreventSettingMode;
+
+        [HarmonyPrefix, HarmonyPriority(Priority.First)]
+        public static bool Prefix() => !PreventSettingMode;
+
+        [HarmonyPostfix]
+        public static void Postfix() => DesiredGameModePatches.ClearCache();
+    }
+}
